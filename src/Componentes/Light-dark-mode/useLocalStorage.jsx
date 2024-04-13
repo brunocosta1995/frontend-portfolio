85e423f6cb66d61ae8b9915f852157d3ac9eda79
Bruno Costa <costabruno.dev@gmail.com>
2024-04-13 10:51:08 -0300

    Atualização

--- src/Componentes/Light-dark-mode/useLocalStorage.jsx
+++ src/Componentes/Light-dark-mode/useLocalStorage.jsx
@@ -1,24 +1,22 @@
 import { useEffect, useState } from "react";
 
 export default function useLocalStorage(key, defaultValue) {
-    let currentValue;
-    const [value, setValue] = useState(() => {
-        try {
-            currentValue = JSON.parse(localStorage.getItem(key) || defaultValue.toString())
+  let currentValue;
+  const [value, setValue] = useState(() => {
+    try {
+      currentValue = JSON.parse(
+        localStorage.getItem(key) || defaultValue.toString()
+      );
+    } catch (error) {
+      console.log(error);
+      currentValue = defaultValue;
+    }
+    return currentValue;
+  });
 
-        } catch (error) {
-            console.log(error);
-            currentValue = defaultValue;
-        }
-        return currentValue;
-    });    
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
 
-    useEffect(()=>{
-
-        localStorage.setItem(key, JSON.stringify(value))
-
-    }, [key, value])
-
-    return [value, setValue];
-
+  return [value, setValue];
 }
